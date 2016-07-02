700709d601955f09d5bb6ae2ddc2075e94c26d72
Sam Bernheim <sambernheim@gmail.com>
2016-07-02 14:00:40 -0400

    added comments to Staircase.java

--- Staircase.java
+++ Staircase.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; 
+
 public class Staircase {
 
     public static void main(String[] args) {
@@ -9,13 +11,20 @@ public class Staircase {
 
     public static void Staircase(int n){
     	
+		// outer for loop to go through each line that will be printed to. 
 		for (int line = 0; line < n; line++) {
-			int spaces; 
-
-			for (spaces = n-line-1; spaces > 0; spaces--){
+			
+			// controls the number of spaces to be printed on each line
+			for (int spaces = n-line; spaces > 1; spaces--){
 				System.out.print(" ");
 			}
-
+			
+			/* 
+			 * Controls the number of # symbols to be printed on each line 
+			 * Start with 1 # on line 1, 2 # on line 2 ... until at the last line
+			 * # is printed a line number of times
+			 * NOTE: do line+1 to offset since line starts at 0 
+			 */
 			for (int star = line+1; star > 0; star--){
 				System.out.print("#");
 			} 
